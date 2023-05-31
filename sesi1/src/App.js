d12d541d7301c86dca831b8adc551c45708206d4
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-05-31 23:39:41 +0700

    alfin-coba reactjs

---
+++ sesi1/src/App.js
@@ -0,0 +1,14 @@
+import Introduction from "./components/introduction";
+import Profile from "./components/profile";
+
+function App() {
+  return (
+    <>
+      <Profile />
+      <Introduction name="alfin budiyanto" />
+      <Introduction name="dea afrizal" />
+    </>
+  );
+}
+
+export default App;
